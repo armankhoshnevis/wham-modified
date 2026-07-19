a342aaab3bc8afe10a99a7403abfa306659b77cd
Arman Khoshnevis <khoshne1@msu.edu>
2026-07-19 11:44:44 -0400

    Add log directory creation to smoke test scripts

--- vampnet/run_c2f_domain_mac_smoke.sh
+++ vampnet/run_c2f_domain_mac_smoke.sh
@@ -1,5 +1,7 @@
 set -o pipefail
 
+mkdir -p runs/mac_smoke/logs
+
 time python scripts/exp/train.py \
   --args.load conf/generated/mac_smoke_domain/c2f.yml \
   2>&1 | tee runs/mac_smoke/logs/domain_c2f.log
--- vampnet/run_c2f_specific_mac_smoke.sh
+++ vampnet/run_c2f_specific_mac_smoke.sh
@@ -1,5 +1,7 @@
 set -o pipefail
 
+mkdir -p runs/mac_smoke/logs
+
 time python scripts/exp/train.py \
   --args.load conf/generated/mac_smoke_species/c2f.yml \
   2>&1 | tee runs/mac_smoke/logs/species_c2f.log
